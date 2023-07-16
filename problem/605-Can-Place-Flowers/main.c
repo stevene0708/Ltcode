b87e605a1268b46902c992d1fe895fe7a9c38a41
StevenWeng <stevene0708@gmail.com>
2023-07-17 01:43:17 +0800

    update 605
    Description:
    	add solution step
    Related Files:
    	problem/605-Can-Place-Flowers/main.c

--- problem/605-Can-Place-Flowers/main.c
+++ problem/605-Can-Place-Flowers/main.c
@@ -20,4 +20,10 @@ bool canPlaceFlowers(int* flowerbed, int flowerbedSize, int n){
     } else {
         return 0;
     }
-}
+} 
+
+// 解題:
+//      在每一個點當下，可以做三種判斷
+//          1. 如果已經種了，就直接判斷i+2的位置
+//			2. 如果i+1位置有種，直接判斷i+3的位置
+//			3. 如果沒有種，n-1再判斷i+2的位置
